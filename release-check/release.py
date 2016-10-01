75176984c7f5a2b73c808423f0275a0c61acb55d
Toma Luca <toma.luca95@gmail.com>
2016-10-01 16:49:26 +0200

    added rechent changes check
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

--- release-check/release.py
+++ release-check/release.py
@@ -6,6 +6,8 @@ import os
 import os.path
 import json
 
+from pprint import pprint
+
 data_dir = "{}/data/".format(os.path.dirname(os.path.realpath(__file__)))
 
 print(data_dir)
@@ -21,19 +23,18 @@ subdomains = ['it', 'en', 'de', 'es', 'fr', 'pt', 'sv', 'ca', 'meta', 'pool']
 global_status = True
 
 for subdomain in subdomains:
+    print(subdomain)
+
     response = requests.get("https://{}{}/api.php?action=query&meta=siteinfo&siprop=statistics&format=json".format(subdomain,domain))
     if 'Content-Type' in response.headers and response.headers['Content-Type']=="application/json; charset=utf-8":
-        print(subdomain)
-
-        file = data_dir+subdomain+domain+".json"
-
+        file_name = data_dir+subdomain+domain+"-siteinfo.json"
         if first_run:
-            text_file = open(file, "wb")
+            text_file = open(file_name, "wb")
             text_file.write(response.content)
             text_file.close()
         else:
             old_data = {}
-            with open(file) as old_file:
+            with open(file_name) as old_file:
                 old_data = json.load(old_file)
                 old_file.close()
             statistics = response.json()['query']['statistics']
@@ -42,10 +43,42 @@ for subdomain in subdomains:
             status = True
             for label in ['activeusers','admins','articles','edits','images','jobs','pages','users']:
                 partial_status = statistics[label] == statistics_old[label]
-                print("{}: {} = {} ({})".format(label,statistics[label],statistics_old[label],partial_status))
+                if not partial_status:
+                    print("{}: {} = {} ({})".format(label,statistics[label],statistics_old[label],partial_status))
                 status = status and partial_status
-            print("Status: {}".format(status))
+            print("\tStatus stats: {}".format(status))
             global_status = global_status and status
-            print()
+
+    response = requests.get("https://{}{}/api.php?action=query&list=recentchanges&rcprop=title%7Cids%7Csizes%7Cflags%7Cuser&rclimit=50&format=json".format(subdomain,domain))
+    if 'Content-Type' in response.headers and response.headers['Content-Type']=="application/json; charset=utf-8":
+        file_name = data_dir+subdomain+domain+"-changes.json"
+        if first_run:
+            text_file = open(file_name, "wb")
+            text_file.write(response.content)
+            text_file.close()
+        else:
+            old_data = {}
+            with open(file_name) as old_file:
+                old_data = json.load(old_file)
+                old_file.close()
+
+            recentchanges_new = response.json()['query']['recentchanges']
+            recentchanges_old = old_data['query']['recentchanges']
+            status_changed = True
+            for i in range(0,len(recentchanges_new)):
+                status = True
+                for key in recentchanges_new[i]:
+                    val_old = None
+                    val_new = None
+                    if key in recentchanges_new[i]:
+                        val_new = recentchanges_new[i][key]
+                    if key in recentchanges_old[i]:
+                        val_old = recentchanges_old[i][key]
+                    status = (status and val_old==val_new)
+                status_changed = status_changed and status
+            print("\tStatus changes: {}".format(status_changed))
+            global_status = global_status and status_changed
+
+
 
 print("Global status: {}".format(global_status))
