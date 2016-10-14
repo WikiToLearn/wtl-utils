924c8cb8bafc37e96fddbb49a6c2595f1ffdad60
Davide Valsecchi <valsecchi.davide94@gmail.com>
2016-10-14 22:38:41 +0200

    Added multilang in config

--- course_editor_templates/src/app.py
+++ course_editor_templates/src/app.py
@@ -11,33 +11,34 @@ import yaml
 
 stream = open('config.yaml', 'r')
 config = yaml.load(stream, Loader=yaml.Loader)
-lang = config["pywikibot"]["lang"]
+langs = config["pywikibot"]["lang"]
 user = config["pywikibot"]["user"]
 passw = config["pywikibot"]["password"]
 
 
 def main():
-    site = pywikibot.Site(lang, "wikitolearn")
-    wtlpywikibot.login(site,user,passw)
-    print("Lang: " + lang )
-    ctempl = pywikibot.Page(site, "Template:CCourse")
+    for lang in langs:
+        site = pywikibot.Site(lang, "wikitolearn")
+        wtlpywikibot.login(site,user,passw)
+        print("Lang: " + lang )
+        ctempl = pywikibot.Page(site, "Template:CCourse")
 
-    print(">>> CCourse fixing...")
-    for p in pg.ReferringPageGenerator(ctempl,
-                    followRedirects=True,withTemplateInclusion=True):
-        print("> ",p.title())
-        p.text = p.text.replace('{{CCourse', "{{CourseRoot")
-        p.text = p.text.replace('{{SSection', "{{CourseLevelTwo")
-        p.text = p.text + "\n<noinclude>[[Category:CourseRoot]]</noinclude>"
-        p.save(botflag=True, minor=True, async=True)
+        print(">>> CCourse fixing...")
+        for p in pg.ReferringPageGenerator(ctempl,
+                        followRedirects=True,withTemplateInclusion=True):
+            print("> ",p.title())
+            p.text = p.text.replace('{{CCourse', "{{CourseRoot")
+            p.text = p.text.replace('{{SSection', "{{CourseLevelTwo")
+            p.text = p.text + "\n<noinclude>[[Category:CourseRoot]]</noinclude>"
+            p.save(botflag=True, minor=True, async=True)
 
-        r = re.compile(r"{{CourseLevelTwo\|(?P<section>.*?)}}")
-        for m in r.finditer(p.text):
-            subp = p.title() + "/" + m.group("section")
-            print(">>> SSection fixing: {}".format(subp))
-            subpage = pywikibot.Page(site, subp)
-            subpage.text = subpage.text + "\n<noinclude>[[Category:CourseLevelTwo]]</noinclude>"
-            subpage.save(botflag=True, minor=True, async=True)
+            r = re.compile(r"{{CourseLevelTwo\|(?P<section>.*?)}}")
+            for m in r.finditer(p.text):
+                subp = p.title() + "/" + m.group("section")
+                print(">>> SSection fixing: {}".format(subp))
+                subpage = pywikibot.Page(site, subp)
+                subpage.text = subpage.text + "\n<noinclude>[[Category:CourseLevelTwo]]</noinclude>"
+                subpage.save(botflag=True, minor=True, async=True)
 
 
 if __name__ == "__main__":
