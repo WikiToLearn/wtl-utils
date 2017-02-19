a6770994be948b011c4fbc653bf33149e0c5a5f2
Toma Luca <toma.luca95@gmail.com>
2017-02-19 15:02:58 +0100

    added --net=host to docker run for full-pages-list
    Signed-off-by: Toma Luca <toma.luca95@gmail.com>

--- full-pages-list/run.sh
+++ full-pages-list/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker build -t wikitolearn/full-pages-list .
 docker run -ti \
+  --net=host \
   -v $(pwd)/config.yaml:/etc/full-pages-list/config.yaml \
   wikitolearn/full-pages-list
